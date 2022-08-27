09153bb2bc5c7f4ce4728a4e41d370ab4feb53cd
Eze Gatica <35713330+ezegatica@users.noreply.github.com>
2022-08-26 23:27:34 -0300

    New auth service

---
+++ auth/src/exceptions.ts
@@ -0,0 +1,54 @@
+export class LoginFailedException extends Response {
+  constructor() {
+    super("Login failed", {
+      status: 401,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+}
+
+export class WrongMethodException extends Response {
+  constructor() {
+    super("Only POST requests are allowed", {
+      status: 405,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+}
+
+export class NotFoundException extends Response {
+  constructor() {
+    super("Could not find the specified resource", {
+      status: 404,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+}
+
+export class WrongProtocolException extends Response {
+  constructor() {
+    super("Only HTTPS connections are allowed", {
+      status: 403,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+}
+
+export class EmptyBodyException extends Response {
+  constructor() {
+    super("No body was specified", {
+      status: 400,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,23 +8,69 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import {
+  EmptyBodyException,
+  LoginFailedException,
+  NotFoundException,
+  WrongMethodException,
+  WrongProtocolException,
+} from "./exceptions";
+import { IUser } from "./interfaces";
+import jwt from "@tsndr/cloudflare-worker-jwt";
+
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  USERS: KVNamespace;
+
+  token: string;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    _ctx: ExecutionContext
+  ): Promise<Response> {
+    const { protocol, pathname } = new URL(request.url);
+
+    if (
+      "https:" !== protocol ||
+      "https" !== request.headers.get("x-forwarded-proto")
+    ) {
+      return new WrongProtocolException();
+    }
+
+    switch (pathname) {
+      case "/":
+        return new Response("welcome");
+      case "/login":
+        if (request.method !== "POST") {
+          return new WrongMethodException();
+        }
+        if (request.headers.get("Content-Type") !== "application/json") {
+          return new EmptyBodyException();
+        }
+        const body = (await request.json()) as IUser;
+        const password = await env.USERS.get(`user:${body.email}`);
+        if (!password || password !== body.password) {
+          return new LoginFailedException();
+        }
+		const secretNullable = await env.USERS.get("secret");
+
+		const secret = JSON.parse(JSON.stringify(secretNullable)) as string;
+
+        const token = await jwt.sign({ email: body.email }, secret, {
+          algorithm: "HS256",
+        });
+
+        return new Response(token);
+
+      default: {
+        return new NotFoundException();
+      }
+    }
+  },
 };
